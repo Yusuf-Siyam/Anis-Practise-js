ba74191edc1a88111bf02d7eb1bf5b4e635cb996
Yusuf-Siyam <mdsiyam1011@gmail.com>
2025-05-15 12:34:24 +0600

    all delet

---
+++ scrip.js
@@ -0,0 +1,18 @@
+var names=["siyam","yusuf","anis","kolpona"];
+console.log(names)
+
+names.push("xxx");
+console.log(names)
+
+names.pop();
+console.log(names);
+
+names.shift();
+console.log(names);
+
+
+names.unshift("sagor");
+console.log(names);
+
+
+
